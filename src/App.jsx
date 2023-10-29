d8c453cc52e687870cfe7cdb294d7a95ec4ae1c6
helllify <138779047+helllify@users.noreply.github.com>
2023-10-29 00:04:20 +0000

    ok

--- src/App.jsx
+++ src/App.jsx
@@ -24,12 +24,12 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
+  name: "Hellify",
+  title: "Make your own paradise",
+  email: "hellify.io@icloud.com",
   gitHub: "microsoft",
   instagram: "microsoft",
-  linkedIn: "satyanadella",
+  linkedIn: "antegeia",
   medium: "",
   twitter: "microsoft",
   youTube: "Code",
